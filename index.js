b4262cf8afe8a321e72ea6b0d585efe3b079aa67
Dolhov Andrey <andrej.dolgov.042110@gmail.com>
2022-05-18 01:25:30 +0300

    correctly work

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 'use strict'
-
+// beginning matrix
 let chess = [
     [0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0],
@@ -9,8 +9,9 @@ let chess = [
     [0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0],
-];
+]; // end of matrix
 
+// drawing field
 function draw() {
     let out = '';
     let m = 0;
@@ -34,8 +35,15 @@ function draw() {
 }
 
 draw();
+// end drawing field
+
+// knight ways how to go
 
 function horse() {
+    document.querySelectorAll('.chess-block').forEach(function(element){
+        element.classList.remove('green');
+        element.classList.remove('active');
+    });
     let x = this.dataset.x;
     let y = this.dataset.y;
     console.log(x + ' ' + y);
@@ -43,6 +51,32 @@ function horse() {
     if (+x + 2 < 8 && +y + 1 < 8) {
         document.querySelector(`[data-x="${+x + 2}"][data-y="${+y + 1}"]`).classList.add('active');
     }
+    if (+x + 2 < 8 && +y - 1 >= 0) {
+        document.querySelector(`[data-x="${+x + 2}"][data-y="${+y - 1}"]`).classList.add('active');
+    }
+    if (+x + 2 < 8 && +y + 2 < 8) {
+        document.querySelector(`[data-x="${+x + 1}"][data-y="${+y + 2}"]`).classList.add('active');
+    }
+    if (+x + 1 < 8 && +y - 2 >= 0) {
+        document.querySelector(`[data-x="${+x + 1}"][data-y="${+y - 2 }"]`).classList.add('active');
+    } // right side
+
+
+    if (+x - 2 >= 0 && +y - 1 >= 0) {
+        document.querySelector(`[data-x="${+x - 2}"][data-y="${+y - 1}"]`).classList.add('active');
+    }
+    if (+x - 1 >= 0 && +y + 2 < 8) {
+        document.querySelector(`[data-x="${+x - 1}"][data-y="${+y + 2}"]`).classList.add('active');
+    }
+    if (+x - 1 >= 0 && +y - 2 >= 0) {
+        document.querySelector(`[data-x="${+x - 1}"][data-y="${+y - 2}"]`).classList.add('active');
+    }
+    if (+x - 2 >= 0 && +y + 2 < 8) {
+        document.querySelector(`[data-x="${+x - 2}"][data-y="${+y + 1 }"]`).classList.add('active');
+    } // left side
+
 
 }
+
+// end knight ways how to go
 
